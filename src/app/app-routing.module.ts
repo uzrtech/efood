a0cd93eef7f53ecce2fb65a00fd8ca4b3bc11850
hamza <uzrtech@gmail.com>
2020-04-21 13:59:13 +0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,22 @@
+import { CourComponent } from './cour/cour.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { CartComponent } from './cart/cart.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomepageComponent},
+ // {path:'', component:CourComponent},
+  {path:'myaccount', component: MyaccountComponent},
+  {path:'cart', component: CartComponent},
+  {path:'about', component: AboutusComponent},
+  {path:'checkout', component: CheckoutComponent},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { MyaccountComponent } from './myaccount/myaccount.component';
+import { CartComponent } from './cart/cart.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { WishlistComponent } from './myaccount/wishlist/wishlist.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { CourComponent } from './cour/cour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    MyaccountComponent,
+    CartComponent,
+    AboutusComponent,
+    WishlistComponent,
+    CheckoutComponent,
+    CourComponent,
   ],
   imports: [
     BrowserModule,
