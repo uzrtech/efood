3ae9efd7968ab0af74cf18d4ba129d2e0884f9a0
hamza <uzrtech@gmail.com>
2020-04-25 17:07:54 +0500

    update

--- src/app/topheader/topheader.component.ts
+++ src/app/topheader/topheader.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
+import {ViewChild, ElementRef, AfterViewInit} from '@angular/core';
 @Component({
   selector: 'app-topheader',
   templateUrl: './topheader.component.html',
   styleUrls: ['./topheader.component.css']
 })
 export class TopheaderComponent implements OnInit {
+  @ViewChild('selectElem') el:ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
+    $("#carousel-home .owl-carousel").on("initialized.owl.carousel", function() {
+      setTimeout(function() {
+        $("#carousel-home .owl-carousel .owl-item.active .owl-slide-animated").addClass("is-transitioned");
+        $("section").show();
+      }, 200);
+    });
   }
 
 }
